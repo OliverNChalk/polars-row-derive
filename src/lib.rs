7e1a39ac07c0d47340ad3d1eea007ec4470df367
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-08-03 16:21:24 -0500

    feat: add to_dataframe_with_prefix

--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,16 @@ pub fn stream_handler_derive(input: TokenStream) -> TokenStream {
 fn impl_handler_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let trait_name_exact = syn::Ident::new(&format!("IterToDataFrame{name}"), name.span());
+    let trait_name_exact_with_prefix =
+        syn::Ident::new(&format!("IterToDataFrame{name}WithPrefix"), name.span());
     let trait_name_dyn = syn::Ident::new(&format!("IterToDataFrameDyn{name}"), name.span());
 
     let doc_msg_exact = format!(
-        "Trait to convert an exact size iterator with elements of type {} into a polars DataFrame.",
-        name
+        "Trait to convert an exact size iterator with elements of type {name} into a polars DataFrame.",
     );
 
     let doc_msg_dyn = format!(
-        "Trait to convert an iterator with elements of type {} into a polars DataFrame.",
-        name
+        "Trait to convert an iterator with elements of type {name} into a polars DataFrame.",
     );
 
     let named_fields = match &ast.data {
@@ -46,6 +46,12 @@ fn impl_handler_macro(ast: &syn::DeriveInput) -> TokenStream {
         #public trait #trait_name_exact {
             #[doc = #doc_msg_exact]
             fn to_dataframe(self) -> Result<polars::frame::DataFrame, polars::prelude::PolarsError>;
+        }
+
+        #[automatically_derived]
+        #public trait #trait_name_exact_with_prefix {
+            #[doc = #doc_msg_exact]
+            fn to_dataframe_with_prefix(self, prefix: String) -> Result<polars::frame::DataFrame, polars::prelude::PolarsError>;
         }
 
         #[automatically_derived]
@@ -73,6 +79,34 @@ fn impl_handler_macro(ast: &syn::DeriveInput) -> TokenStream {
                 polars::df! {
                     #(stringify!(#named_fields) => #named_fields,)*
                 }
+            }
+        }
+
+        #[automatically_derived]
+        impl<T> #trait_name_exact_with_prefix for T
+        where
+            T: Iterator<Item = #name> + ExactSizeIterator,
+        {
+            fn to_dataframe_with_prefix(self, prefix: String) -> Result<polars::frame::DataFrame, polars::prelude::PolarsError> {
+
+                let len = self.len().to_owned();
+
+                // Extract the field values into separate vectors
+                #(let mut #named_fields = Vec::with_capacity(len);)*
+
+
+                for e in self.into_iter() {
+                    #(#named_fields.push(e.#named_fields);)*
+                }
+
+                polars::frame::DataFrame::new(
+                    vec![
+                        #(polars::frame::column::Column::new(
+                            format!("{prefix}_{}", stringify!(#named_fields)).into(),
+                            #named_fields
+                        ),)*
+                    ]
+                )
             }
         }
 
